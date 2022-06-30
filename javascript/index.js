d1bbc8cfc58b05738edfdeceb63a5adcf6e9bfdb
ketan chouhan <ketan120901@gmail.com>
2022-06-30 13:13:13 +0530

    lastchange

--- javascript/index.js
+++ javascript/index.js
@@ -33,24 +33,24 @@ function initMap() {
 
                     });
                     const infowindow = new google.maps.InfoWindow({
-                        content:`latitude: ${element.latitude}<br>
-                        pop: ${element.pop}<br>
+                        content: `latitude: ${element.latitude}<br>
+                        
                         longitude: ${element.longitude}<br>
                         name: ${element.name}<br>
                         country: ${element.country}<br>
                         lastUpdated: ${element.lastUpdated}<br>
                         dead: ${element.dead}<br>
-                        id: ${element.id}<br>
+                        
                         infected: ${element.infected}<br>
                         recovered: ${element.recovered}`
-                      });
-                    
-                      marker.addListener("mouseover", () => {
+                    });
+
+                    marker.addListener("mouseover", () => {
                         infowindow.open(marker.get("map"), marker);
-                      });
-                      marker.addListener("mouseout", () => {
+                    });
+                    marker.addListener("mouseout", () => {
                         infowindow.close(marker.get("map"), marker);
-                      });
+                    });
                 });
             })
     }
